e3d530fdff150431960a73669bd462a56030718d
Herviyana <herviyana0@gmail.com>
2025-09-23 17:12:01 +0700

    changes publik to public

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const CONFIG = {
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "publik")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // MQTT Configuration
 const MQTT_BROKER = "mqtt://test.mosquitto.org";
